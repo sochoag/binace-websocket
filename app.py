6d5b281de6ee5984d8c480563c5b0f4e9c7875c9
sochoag <santiagodav@hotmail.es>
2024-05-05 17:38:33 -0500

    fix: :bug: Resolving conflict inserting Data and connecting

--- app.py
+++ app.py
@@ -3,42 +3,46 @@ import websockets
 import os
 from publishData import *
 from getData import get_data
-import json
 
-async def receive_and_publish_data(websocket, path):
-    print(path)
-    if path == "/":
+async def upload_data():
+  binance_socket = await websockets.connect('wss://fstream.binance.com/stream?streams=btcusdt@kline_1m/bnbusdt@kline_1m/ethusdt@kline_1m/solusdt@kline_1m/xrpusdt@kline_1m')
+  try:  
+    while True:
+      data = await get_data(binance_socket)
+      if data:
+        coin = await insert_data(data)
+        print("Inserting data into: "+coin)
+  except:
+    print("Error with Binance connection")
+  finally:
+    await binance_socket.close()
 
-      coins = ['BTCUSDT', 'BNBUSDT', 'ETHUSDT', 'SOLUSDT', 'XRPUSDT']
-
-      for coin in coins:
-        response = await get_last_ten_records(coin)
-        await publish_data(websocket, response)
-
-      async with  websockets.connect('wss://fstream.binance.com/stream?streams=btcusdt@kline_1m/bnbusdt@kline_1m/ethusdt@kline_1m/solusdt@kline_1m/xrpusdt@kline_1m') as binance_socket:
-          try:
-            while True:
-              data = await get_data(binance_socket)
-              if data:
-                coin = await insert_data(data)
-                response = await get_last_ten_records(coin)
-                await publish_data(websocket, response)
-          except websockets.exceptions.ConnectionClosedOK:
-            print("No clients connected. Waiting for connections...")
-            
-    if path == "/BTCUSDT":
-      response = await get_last_ten_records()
-      await publish_data(websocket, response)
+async def handler(websocket):
+  coins = ['BTCUSDT', 'BNBUSDT', 'ETHUSDT', 'SOLUSDT', 'XRPUSDT']
+  try:
+    while True:
+        for coin in coins:
+          response = await get_last_ten_records(coin)
+          await publish_data(websocket, response)
+        await asyncio.sleep(60)
+  except websockets.exceptions.ConnectionClosedOK:
+    print("No clients connected. Waiting for connections...")
 
 
 async def start_server():
     port = int(os.environ.get("PORT", 8000))
     try:
       print("WebSocket server started. Waiting for connections...")
-      async with websockets.serve(receive_and_publish_data, '0.0.0.0', port):
+      async with websockets.serve(handler, '0.0.0.0', port):
           await asyncio.Future()
     except:
       print("Server shutting down...")
 
+
+async def main():
+  server_task = asyncio.create_task(start_server())
+  data_task = asyncio.create_task(upload_data())
+  await asyncio.gather(data_task, server_task)
+
 if __name__ == "__main__":
-    asyncio.run(start_server())
+    asyncio.run(main())
